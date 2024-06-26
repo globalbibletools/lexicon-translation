f266cbb18707a1ba150a920352dfde2d3008a4ac
ilearner777 <ilearner@protonmail.com>
2024-06-26 09:16:53 -0400

    Refactored and tested Hebrew sample

--- scripts/extract-byWord-treeItems.js
+++ scripts/extract-byWord-treeItems.js
@@ -12,19 +12,27 @@
 // - "Lemma":
 // - "AlphaPos":
 
-// The data extracted will be used to create a JSON format as follows:
+// The logic employed in this script will allow for the creation of a treeView
+// with a max of 9 levels and a max of 999 items at each level.
+
+// The logic is based on the assumption that the input file is sorted by the lemma field.
+
+// The format used for the key field in the treeItem was derived from
+// the "code" field in the USB Dictionary Lexical Domains and Contextual Domains files.
+
+// The data extracted will be used to create a JSON file with format as follows:
 // [
 //   {
 //     level: Number,
 //     key: string,
 //     label: string,
-//     command: string,
+//     fileName: string,
 //   },
 //   {
 //     level: Number,
 //     key: string,
 //     label: string,
-//     command: string,
+//     fileName: string,
 //   }
 // ];
 
@@ -33,7 +41,7 @@
 //       the key field will be used as the key in the map.
 //       the value of the map will be an instance of treeItem.
 //       The key of the map is used to allow for easy access of the children of treeItem selected by the user.
-//       The key is a string where the positions of the characters will indicate contain specific information
+//       The key is a string where the positions of the characters will contain specific information
 //       Position: 1 - will indicate the level of the treeItem
 //       Position: 2 - will indicate the language: "H" for Hebrew, "G" for Greek
 //       Position: 3 to 5 - will be a 3 character number that will be used to sort the treeItems for level 1
@@ -44,8 +52,8 @@
 //           follow by "H" or "G" for Hebrew or Greek
 //           followed by a 3 character key: 001, 002, 003, etc to allow for sorting
 //           Therefore the first item in a Hebrew treeView would be "1H001"
-//       example: level 2 items will start with the number "2" followed by the level 1 key plus a 3 character key as used in level 1:
-//           and then next 3 characters will be the sort order of the 2nd level item
+//       example: level 2 items will start with the number "2" followed by the level 1 key plus an additional 3 character numeric value:
+//           that will be the sort order of the 2nd level item
 //           example first Hebrew item under level one is key: 2H0001001,
 //                   2nd item under level one is key: 2H001002
 //           The above pattern is repeated for all 3 levels
@@ -53,30 +61,30 @@
 //  - label: Will contain the text that is displayed in the tree item view
 //        for level 1 it will contain: "Hebrew"
 //        for level 2 it will contain: the contents of "AlphaPos"
-//        for level 3 it will contain: the first 2 letters of "Lemma" field. (local variable alphaBetaPos)
+//        for level 3 it will contain: the first 2 letters of the "Lemma" field. (local variable alphaBetaPos)
 //            NOTE: the Hebrew the language is in the reverse direction of Greek
 //        for level 4: it will contain the entire contents of "Lemma" field.
-//  - command: only applies to items will execute a command when selected by the user.
+//  - fileName: only applies to items that will execute a command when selected by the user.
 //        In this application it only applies to level 4 and will contain the first 6 characters of the "MainId" field,
 //        which is the file name minus the extension.
-//        This will be used to open the file and position the cursor at the beginning of the word.
+//        This will be used to open the file for that word.
 //
 // Each Dictionary entry in the input file contains the information to create levels 2-4 in the treeView.
 // However, we will only store the information for each level once in the output file.
 // Therefore they will not be duplicated in the output file.
 //
-// The script will determine where the language is Hebrew or Greek based on the name of the input file.
+// The script will determine whether the language is Hebrew or Greek based on the name of the input file.
 // If the input file name contains "Hebrew" then the language will be Hebrew.
 // If the input file name contains "Greek" then the language will be Greek.
 
 // As the routine processes the contents of the JSON input file,
-// it will be stored into a new map with the key field as the key of the map.
+// the required data will be stored into a new map with the key field as the key of the map.
 // and the value being a Javascript object with this definition:
 // {
 //   level: Number;
 //   key: string;
 //   label: string;
-//   command: string;
+//   fileName: string;
 // }
 
 import fs from "node:fs";
@@ -84,8 +92,6 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 import console from "node:console";
 import { exit } from "node:process";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // See https://nodejs.org/docs/latest/api/process.html#processargv
 const args = process.argv.slice(2);
@@ -96,7 +102,10 @@ if (args.length < 1) {
   process.exit(1);
 }
 
+// Set global constants
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const jsonFilePath = process.argv[2];
+const resultMap = new Map();
 
 // Set variables based on the language of the input file
 let language;
@@ -104,6 +113,7 @@ let languageCode;
 let l1Count;
 let outputFile;
 let diacriticsRegex;
+
 if (jsonFilePath.toLowerCase().includes("hebrew")) {
   language = "Hebrew";
   languageCode = "H"; // Added to the key to indicate Hebrew
@@ -123,7 +133,6 @@ if (jsonFilePath.toLowerCase().includes("hebrew")) {
     "FATAL ERROR: \nInvalid file name. Language could not be determined.\n Must contain either 'Hebrew' or 'Greek' in the file name."
   );
 }
-
 const outputPath = path.join(__dirname, outputFile);
 
 // Read and process the JSON file
@@ -135,7 +144,6 @@ fs.readFile(jsonFilePath, { encoding: "utf8" }, (err, data) => {
 
   try {
     const entries = JSON.parse(data);
-    const resultMap = new Map();
     let level = 1;
     let parentKey = "";
     let key = "";
@@ -151,12 +159,7 @@ fs.readFile(jsonFilePath, { encoding: "utf8" }, (err, data) => {
 
     // Lets build the first level which simply contains the word "Hebrew" or "Greek"
     key = _nextKey(level, parentKey, l1Count);
-    resultMap.set(key, {
-      level: 1,
-      key: key,
-      label: language,
-      command: "",
-    });
+    _storeTreeItem(level, key, language, "");
 
     // Let's build the rest of the tree (Level's 2 to 4) by detecting when we change from one level to another
     entries.forEach((entry) => {
@@ -175,26 +178,14 @@ fs.readFile(jsonFilePath, { encoding: "utf8" }, (err, data) => {
         alphaPos = entry.AlphaPos;
         parentKey = _parentKey(level, key);
         key = _nextKey(level, parentKey, l2Count);
-        if (!resultMap.has(key)) {
-          resultMap.set(key, {
-            level: level,
-            key: key,
-            label: entry.AlphaPos,
-            command: "",
-          });
-        }
+        _storeTreeItem(level, key, alphaPos, "");
 
         // store level 3 treeItem
         level = 3;
         l3Count++;
         parentKey = _parentKey(level, key);
         key = _nextKey(level, parentKey, l3Count);
-        resultMap.set(key, {
-          level: level,
-          key: key,
-          label: alphaBetaPos,
-          command: "",
-        });
+        _storeTreeItem(level, key, alphaBetaPos, "");
         prevAlphaBetaPos = alphaBetaPos;
 
         // store level 4 treeItem
@@ -202,12 +193,7 @@ fs.readFile(jsonFilePath, { encoding: "utf8" }, (err, data) => {
         l4count++;
         parentKey = _parentKey(level, key);
         key = _nextKey(level, parentKey, l4count);
-        resultMap.set(key, {
-          level: level,
-          key: key,
-          label: lemma,
-          command: fileName,
-        });
+        _storeTreeItem(level, key, lemma, fileName);
 
         // See if there is a change in the AlphaBetaPos field to indicate a new level 3 item
       } else if (prevAlphaBetaPos !== alphaBetaPos) {
@@ -215,12 +201,7 @@ fs.readFile(jsonFilePath, { encoding: "utf8" }, (err, data) => {
         l3Count++;
         parentKey = _parentKey(level, key);
         key = _nextKey(level, parentKey, l3Count);
-        resultMap.set(key, {
-          level: level,
-          key: key,
-          label: alphaBetaPos,
-          command: "",
-        });
+        _storeTreeItem(level, key, alphaBetaPos, "");
         prevAlphaBetaPos = alphaBetaPos;
 
         // store level 4 treeItem
@@ -228,24 +209,14 @@ fs.readFile(jsonFilePath, { encoding: "utf8" }, (err, data) => {
         l4count++;
         parentKey = _parentKey(level, key);
         key = _nextKey(level, parentKey, l4count);
-        resultMap.set(key, {
-          level: level,
-          key: key,
-          label: lemma,
-          command: fileName,
-        });
+        _storeTreeItem(level, key, lemma, fileName);
       } else {
         // store level 4 treeItem
         level = 4;
         l4count++;
         parentKey = _parentKey(level, key);
         key = _nextKey(level, parentKey, l4count);
-        resultMap.set(key, {
-          level: level,
-          key: key,
-          label: lemma,
-          command: fileName,
-        });
+        _storeTreeItem(level, key, lemma, fileName);
       }
     });
 
@@ -301,7 +272,18 @@ function _slice(language, string) {
   // Get the appropriate two grapheme clusters
   const alphaBetaPos =
     language === "Hebrew"
-      ? segments.slice(-2).join("")
-      : segments.slice(0, 2).join("");
+      ? segments.slice(-2).join("") + "..."
+      : segments.slice(0, 2).join("") + "...";
   return alphaBetaPos;
+}
+
+function _storeTreeItem(level, key, label, fileName) {
+  if (!resultMap.has(key)) {
+    resultMap.set(key, {
+      level: level,
+      key: key,
+      label: label,
+      fileName: fileName,
+    });
+  }
 }
